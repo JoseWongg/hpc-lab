8383104f4fd9c96a7f05740c365ad26582abdf4e
JoseWongg <j.wong@mail.com>
2026-04-10 00:27:20 +0100

    Update gitignore and start script

--- scripts/start-hpc-lab.ps1
+++ scripts/start-hpc-lab.ps1
@@ -2,8 +2,8 @@
 
 # How to run:
 # 1. Open PowerShell
-# 2. Connect to Azure with `az login`
-# 3. Run this script with `.\start-hpc-lab.ps1`
+# 2. Connect to Azure with `az login`. To connect type `az login --use-device-code` and follow the instructions to authenticate with your Azure account. Make sure you have access to the subscription where the HPC Lab resources are located.
+# 3. Run this script with `.\scripts\start-hpc-lab.ps1`
 
 az vm start --resource-group HPC_Lab --name hpc-login-ctl
 az vm start --resource-group HPC_Lab --name hpc-compute-01
